2a22361e2376b568060deb986c5f2afa45b4d64f
stonarini <stonarini@cifpfbmoll.eu>
2021-11-29 19:43:00 +0100

    feat(scrap_price): add method to scrap the price of an item

---
+++ src/book_scraper/get_book_dynamic_data/scrap_price.py
@@ -0,0 +1,17 @@
+from src.book_scraper.get_book_dynamic_data.amazon_scraper_config import price_tag
+
+
+def scrap_price(soup):
+    try:
+        price = (
+            soup.find(
+                price_tag["tag_name"],
+                attrs={price_tag["tag_type"]: price_tag["tag_id"]},
+            )
+            .string.strip()
+            .replace(",", "")
+        )
+    except:
+        price = "Na"
+    finally:
+        return price
