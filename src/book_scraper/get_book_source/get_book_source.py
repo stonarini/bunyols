12c922659acb9465720eb7075de4746b8e34d8c0
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2021-11-29 20:33:52 +0100

    refactor: run black in 3 existing files

--- src/book_scraper/get_book_source/get_book_source.py
+++ src/book_scraper/get_book_source/get_book_source.py
@@ -1,11 +1,10 @@
-from bs4 import BeautifulSoup
 from src.book_scraper.get_book_source.get_page_content import get_page_content
 
 
 def get_book_source(URL):
-    
-    assert isinstance(URL, str), 'URL should be str type object'
-    
+
+    assert isinstance(URL, str), "URL should be str type object"
+
     webpage = get_page_content(URL)
 
     if webpage:
--- test/book_scraper/test_get_book_isbn.py
+++ test/book_scraper/test_get_book_isbn.py
@@ -3,13 +3,18 @@ from src.book_scraper.get_book_isbn import get_book_isbn
 from src.book_scraper.get_book_source.get_book_source import get_book_source
 
 
-website = get_book_source("https://www.amazon.com/Code-Complete-Practical-Handbook-Construction/dp/0735619670/")
+website = get_book_source(
+    "https://www.amazon.com/Code-Complete-Practical-Handbook-Construction/dp/0735619670/"
+)
+
 
 @pytest.mark.test_isbn_is_string
 def test_isbn_is_string():
-    assert isinstance(get_book_isbn(website), str), 'ISBN has to be a string type object'
+    assert isinstance(
+        get_book_isbn(website), str
+    ), "ISBN has to be a string type object"
+
 
 @pytest.mark.test_isbn_lenght
-def test_isbn_lenght(): 
+def test_isbn_lenght():
     assert len(get_book_isbn(website)) == 14 or 13
-    
