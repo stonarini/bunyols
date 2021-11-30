53c9ce4e1f482b7e880d108154e897fbdbaee482
stonarini <stonarini@cifpfbmoll.eu>
2021-11-30 19:55:20 +0100

    feat(get_total_reviews): add method to scrap the total number of reviews of a webpage

---
+++ src/book_scraper/get_book_dynamic_data/get_total_reviews.py
@@ -0,0 +1,17 @@
+from src.book_scraper.get_book_dynamic_data.amazon_scraper_config import reviews_tag
+
+
+def get_total_reviews(soup):
+    try:
+        total_reviews = (
+            soup.find(
+                reviews_tag["tag_name"],
+                attrs={reviews_tag["tag_type"]: reviews_tag["tag_id"]},
+            )
+            .string.strip()
+            .replace(",", "")
+        )
+    except TypeError:
+        return "Na"
+    else:
+        return int(total_reviews.split(" ")[0])
