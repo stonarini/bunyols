fba46932e011f252756dd3fc9745a9603a0415cd
stonarini <stonarini@cifpfbmoll.eu>
2021-11-30 20:17:24 +0100

    chore: update pytest.ini and amazon elements

--- src/book_scraper/get_book_dynamic_data/amazon_scraper_config.py
+++ src/book_scraper/get_book_dynamic_data/amazon_scraper_config.py
@@ -3,3 +3,15 @@ price_tag = {
     "tag_type": "class",
     "tag_id": "a-color-price",
 }
+
+reviews_tag = {
+    "tag_name": "span",
+    "tag_type": "id",
+    "tag_id": "acrCustomerReviewText",
+}
+
+star_tag = {
+    "tag_name": "a",
+    "tag_type": "class",
+    "tag_id": "a-link-normal",
+}
