49af56c4bcb5c8977ea9c5491909359e84a7a1dd
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2021-12-01 19:57:13 +0100

    fix(get_book_dynamic_data): convert content to BeautifulSoup object

--- src/book_scraper/get_book_dynamic_data/get_book_dynamic_data.py
+++ src/book_scraper/get_book_dynamic_data/get_book_dynamic_data.py
@@ -1,8 +1,10 @@
+from bs4 import BeautifulSoup
 from .get_book_price import get_book_price
 from .get_book_reviews import get_book_reviews
 
 
-def get_book_dynamic_data(soup):
+def get_book_dynamic_data(content):
+    soup = BeautifulSoup(content, "lxml")
     price = get_book_price(soup)
     reviews = get_book_reviews(soup)
     return {"price": price, "reviews": reviews}
