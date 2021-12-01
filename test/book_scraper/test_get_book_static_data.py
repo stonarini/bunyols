fbf3e5e6d65c7d12626c4e3ab5c5d1ed1387fdeb
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2021-12-01 19:01:33 +0100

    test(test_get_book_static_data): add test to check correct argumen if not raise an exception

--- test/book_scraper/test_get_book_static_data.py
+++ test/book_scraper/test_get_book_static_data.py
@@ -2,8 +2,10 @@ import pytest
 from src.book_scraper.get_book_static_data.request_openlibary_page import (
     request_openlibrary_page,
 )
-
-# #que devuelva un diccionario, devuelva dicc con keys title, author, publisher..., author no sea igual a none
+from src.book_scraper.get_book_static_data.get_book_static_data import (
+    get_book_static_data,
+)
+from src.book_scraper.get_book_source.exceptions import StatusCodeException
 
 
 @pytest.mark.test_static_data_is_dict
@@ -11,6 +13,17 @@ def test_static_data_dicc():
     assert isinstance(request_openlibrary_page("books/OL7353617M"), dict)
 
 
-# @pytest.mark.test_static_data_dicc_keys
-# def test_static_data_dicc_keys():
-#     assert
+data = "/isbn/97805903427"
+
+
+@pytest.mark.test_get_book_static_data
+def test_get_book_static_data():
+    try:
+        get_book_static_data(data)
+    except StatusCodeException:
+        assert True
+    else:
+        assert False
+
+
+
