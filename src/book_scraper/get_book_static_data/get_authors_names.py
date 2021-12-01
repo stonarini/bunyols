afd4cfbbb94e06fda6dec229c682c602bb666b21
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2021-12-01 17:15:44 +0100

    refactor: delete native requests sentences and use for loop

--- src/book_scraper/get_book_static_data/get_authors_names.py
+++ src/book_scraper/get_book_static_data/get_authors_names.py
@@ -4,24 +4,15 @@ from src.book_scraper.get_book_static_data.get_authors_address import (
 from src.book_scraper.get_book_static_data.request_openlibary_page import (
     request_openlibrary_page,
 )
-import requests, json
 
 
-def get_authors_names(authors_list):
+def get_authors_names(work_address):
+    work = request_openlibrary_page(work_address)
+    author_pages = get_authors_address(work["authors"])
 
-    authors_names = []
-    incrementator = 0
-    while incrementator != len(authors_list):
-        authors_info = requests.get(
-            "https://openlibrary.org" + authors_list[incrementator] + ".json"
-        ).content
-        authors_info = json.loads(authors_info)
-        authors_names.append(authors_info["name"])
-        incrementator += 1
+    author_names = []
+    for author in author_pages:
+        author_info = request_openlibrary_page(author)
+        author_names.append(author_info["name"])
 
-    return authors_names
-
-
-content = request_openlibrary_page("/works/OL8305641W")
-list = get_authors_address(content)
-get_authors_names(list)
+    return author_names
