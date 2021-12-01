3b4afeda5fed6acc0c0486d5e2bb188fd34246e1
stonarini <stonarini@cifpfbmoll.eu>
2021-12-01 19:02:17 +0100

    test(get_book_dynamic_data): add test for new method

---
+++ test/book_scraper/get_book_dynamic_data/test_get_book_dynamic_data.py
@@ -0,0 +1,36 @@
+import pytest
+from bs4 import BeautifulSoup
+from src.book_scraper.get_book_dynamic_data import get_book_dynamic_data
+
+
+@pytest.mark.test_dynamic_data_input_is_valid
+def test_dynamic_data_input_is_valid():
+    soup = BeautifulSoup(
+        '<span class="a-color-price">\n$36.48\n</span> \
+        <span id="acrCustomerReviewText">114 reviews</span> \
+        <a class="a-link-normal 5star" title="5 stars represent 77% of rating"></a> \
+        <a class="a-link-normal 4star" title="4 stars represent 15% of rating"></a> \
+        <a class="a-link-normal 3star" title="3 stars represent 2% of rating"></a> \
+        <a class="a-link-normal 2star" title="2 stars represent 1% of rating"></a> \
+        <a class="a-link-normal 1star" title="1 stars represent 6% of rating"></a> \
+        ',
+        "lxml",
+    )
+    book_data = get_book_dynamic_data(soup)
+    assert book_data["price"][0]["value"] == "$36.48"
+    assert book_data["reviews"] == {
+        "total_reviews": 114,
+        "5": 88,
+        "4": 17,
+        "3": 2,
+        "2": 1,
+        "1": 6,
+    }
+
+
+@pytest.mark.test_dynamic_data_input_is_not_valid
+def test_dynamic_data_input_is_not_valid():
+    soup = "price and reviews are none"
+    book_data = get_book_dynamic_data(soup)
+    assert book_data["price"][0]["value"] == "Na"
+    assert book_data["reviews"] == {"total_reviews": "Na"}
