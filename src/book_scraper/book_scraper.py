11b429aed223a367a4da31da46133545e5f3c66a
stonarini <stonarini@cifpfbmoll.eu>
2021-12-03 02:44:52 +0100

    fix: fix typo

--- src/book_scraper/book_scraper.py
+++ src/book_scraper/book_scraper.py
@@ -1,12 +1,5 @@
 from .get_book_source import get_book_source
-<<<<<<< HEAD
-from .get_book_isbn import get_book_isbn
-from .get_book_dynamic_data import get_book_dynamic_data
-from .get_categories import get_categories
-from src.database import create_one
-=======
 from .get_book_data import get_book_data
->>>>>>> book-scraper
 
 
 def book_scraper(items):
@@ -14,15 +7,5 @@ def book_scraper(items):
         URL, item = item
         content = get_book_source(URL)
 
-<<<<<<< HEAD
-        static_data = get_book_static_data("/isbn/" + ISBN)
-        dynamic_data = get_book_dynamic_data(content)
-        categories = get_categories(family, topics)
-
-        book_data = {**static_data, **dynamic_data, **categories}
-
-        create_one(book_data)
-=======
         book_data = get_book_data(content, item)
         print(book_data)
->>>>>>> book-scraper
