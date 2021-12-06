5992d0b31090d36790f3d9944aed07e5afa39109
stonarini <stonarini@cifpfbmoll.eu>
2021-12-06 18:29:27 +0100

    fix: make method search for ISBN-13 instead of just ISBN

--- src/get_book_isbn.py
+++ src/get_book_isbn.py
@@ -1,6 +1,6 @@
 def get_book_isbn(website):
     website_content = str(website)
-    start = website_content.find("ISBN")
+    start = website_content.find("ISBN-13")
     start = website_content.find("9", start)
     ISBN = website_content[start : start + 14]
 
