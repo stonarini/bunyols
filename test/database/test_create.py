0c142ea6a83e38790594d26a467d1242fded2e3c
stonarini <stonarini@cifpfbmoll.eu>
2021-12-06 18:30:47 +0100

    test(database): create and partially fix database tests

--- test/database/test_create.py
+++ test/database/test_create.py
@@ -1,13 +1,23 @@
-from pymongo.errors import WriteError
 import pytest
-from src.database.create import create_one
+import src.database as database
 
 
 @pytest.mark.test_create_invalid_document
 def test_create_invalid_document():
-    try:
-        create_one({"test": 1})
-    except WriteError:
-        assert True
-    else:
-        assert False
+    database.create_one({"test": 1})
+
+
+@pytest.mark.test_create_document
+def test_create_document():
+    database.create_one(
+        {
+            "title": "title",
+            "author": ["author"],
+            "publisher": "publisher",
+            "ISBN_13": "978",
+            "publish_date": "",
+            "price": [{"value": "$00", "date": "someday"}],
+            "reviews": {"total_reviews": 80, "5": 5, "4": 4, "3": 3, "2": 2, "1": 1},
+            "categories": ["CAT"],
+        }
+    )
