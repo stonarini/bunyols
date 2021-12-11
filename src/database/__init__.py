9ad328685d1a887f6739e791fd0881657eb12f17
stonarini <stonarini@cifpfbmoll.eu>
2021-12-11 17:15:00 +0100

    fix: fix import paths

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,4 +1,4 @@
 from .create import create_one
 from .update import update_one
-from .find import find_one
+from .find import find_one, find_all
 from .delete import delete_one
--- src/database/find.py
+++ src/database/find.py
@@ -1,3 +1,4 @@
+import database
 from .connection import connection
 
 
@@ -5,3 +6,9 @@ def find_one(query):
     database = connection()
     book = database.find_one(query)
     return book
+
+
+def find_all():
+    database = connection()
+    books = database.find()
+    return books
