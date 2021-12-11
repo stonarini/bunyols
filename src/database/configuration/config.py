8591908aa0d7b4b403bf7458a314fdc8784e1b30
stonarini <stonarini@cifpfbmoll.eu>
2021-12-11 17:26:14 +0100

    feat(delete): add delete method

---
+++ src/database/configuration/config.py
@@ -0,0 +1,4 @@
+USERNAME = "eze"
+PASSWORD = "eze"
+DATABASE = "bunyols_library"
+URI = f"mongodb+srv://{USERNAME}:{PASSWORD}@bunyols-cluster0.fjgny.mongodb.net/{DATABASE}?retryWrites=true&w=majority"
---
+++ src/database/find.py
@@ -0,0 +1,14 @@
+import database
+from .connection import connection
+
+
+def find_one(query):
+    database = connection()
+    book = database.find_one(query)
+    return book
+
+
+def find_all():
+    database = connection()
+    books = database.find()
+    return books
