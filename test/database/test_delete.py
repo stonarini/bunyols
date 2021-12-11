788edfae13dec6d578e748819ac2021892dc4842
stonarini <stonarini@cifpfbmoll.eu>
2021-12-11 17:12:01 +0100

    test: fix delete test

--- test/database/test_delete.py
+++ test/database/test_delete.py
@@ -5,8 +5,9 @@ import src.database as database
 
 @pytest.mark.test_delete
 def test_delete():
-    database.delete_one({"title":"title"})
-    
+    database.delete_one({"title": "title"})
+
+
 @pytest.mark.test_invalid_delete
 def test_invalid_delete():
-    database.delete_one({"title":"title don't exist"})
+    database.delete_one({"title": "title don't exist"})
