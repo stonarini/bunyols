c762c671d08601774d87dac7ceaeae990d8e6dcf
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2021-12-13 16:06:09 +0100

    chore: remove innecesary import

--- src/utilities/__init__.py
+++ src/utilities/__init__.py
@@ -1,5 +1,4 @@
 from .get_book_isbn import get_book_isbn
-from .get_categories import get_categories
 from .write_to_file import write_to_file
 from .update_bunyol import update_bunyol
 from .create_bunyol import create_bunyol
