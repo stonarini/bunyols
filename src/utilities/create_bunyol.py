cfdef0aa8bc1b75f864e1be25bd1e4ef4490aa92
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2021-12-13 16:08:21 +0100

    fix(create_bunyol): delete redundant import

--- src/utilities/create_bunyol.py
+++ src/utilities/create_bunyol.py
@@ -1,9 +1,10 @@
-from src import book_scraper, database, utilities
+from src import book_scraper, database
+from .get_categories import get_categories
 
 
 def create_bunyol(ISBN, content, categories):
     book_data = book_scraper.create_book_data(content, ISBN)
     if book_data:
-        categories = utilities.get_categories(*categories)
+        categories = get_categories(*categories)
         book_data.update(**categories)
         database.create_one(book_data)
